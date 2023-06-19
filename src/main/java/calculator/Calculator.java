57a31becf0397179f33bd6ef9b494d54ef6bbade
Pietro Giordano <giopi87@gmail.com>
2023-06-19 14:33:22 +0200

    classe calculator + inizializzazione calculator test

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,2 +1,23 @@
-package calculator;public class Calculator {
+package calculator;
+
+public class Calculator {
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float substract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new ArithmeticException("Divisione per 0 non consentita");
+        }
+    }
 }
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void substract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+    }
+}
